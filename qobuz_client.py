56f57f85ec0c44e04205aeaaf443284ba32eda85
Chi Nguyen <buichinguyen1105@gmail.com>
2025-12-13 00:32:28 +0200

    QobuzClient protocol

--- qobuz_client.py
+++ qobuz_client.py
@@ -2,8 +2,8 @@ from typing import Protocol
 from data_models import Album, Quality, ResolvedDownload
 
 class QobuzClient(Protocol):
-    def get_album(album_id: str) -> Album:
-        pass
+    def get_album(self, album_id: str) -> Album:
+        ...
 
-    def resolve_download(track_id: str, quality: Quality) -> ResolvedDownload:
-        pass
+    def resolve_download(self, track_id: str, quality: Quality) -> ResolvedDownload:
+        ...
